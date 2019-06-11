6e5d978abc9a50fa005f68e90ae10ddbad3202bb
Leandro <llimas@outlook.com>
2019-06-10 21:59:06 -0300

    make shadow effect in image

--- win-background-changer.js
+++ win-background-changer.js
@@ -16,22 +16,37 @@ console.log('Interval in seconds to switch the background:', intervalInSeconds, 
 function getRandomIndexArray(randomNumber, arrLength){
     return Math.floor(randomNumber * arrLength);
 }
+/** Necessary to make "shadow effect" with a hack */
+function writeOnImage(image, font, phrase){
+    let timesToWrite = 2;
+    for (let index = 0; index < timesToWrite; index++) {
+        image.print(font[0], index, 1, {
+            text : phrase,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+            }, image.bitmap.width, image.bitmap.height
+        );
+    }
+    image.print(font[1], 3, 1, {
+        text : phrase,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        }, image.bitmap.width, image.bitmap.height
+    );
+}
 async function main(){
     let randomNumber = Math.random();
     let randomNumberArrPhrases = getRandomIndexArray(randomNumber, phrasesArray.phrases.length);
     let randomNumberArrImg = getRandomIndexArray(randomNumber, arrImagesName.length);
-    let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
     let image = await Jimp.read('./images/' + arrImagesName[randomNumberArrImg]);
-    image.print(font, 50, 50, {
-            text : phrasesArray.phrases[randomNumberArrPhrases],
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-        }, image.bitmap.width, image.bitmap.height
-    );
+    let fontBlack = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    let fontWhite = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    writeOnImage(image, [fontBlack, fontWhite], phrasesArray.phrases[randomNumberArrPhrases]);
     image.write('./images/imgwrite.jpeg');
     await wallpaper.set('./images/imgwrite.jpeg');
-    console.log('Background image changed', moment().format('hh:mm:ss'));
+    console.log('Background image changed in', moment().format('hh:mm:ss'));
     console.log('Phrase written:', phrasesArray.phrases[randomNumberArrPhrases]);
     console.log('Next image will be changed in', intervalInSeconds, 'seconds.');
 }
+main();
 setInterval(main,miliseconds);
